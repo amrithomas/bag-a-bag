f2ad3cd0527799eae53e49c005e1f35ecefee9bc
Breno-S <breno_santana_silva@hotmail.com>
2023-04-27 17:24:03 -0300

    Create dados_passagem.php

---
+++ back/dados_passagem.php
@@ -0,0 +1,17 @@
+<?php
+session_start();
+include_once("conexao.php");
+
+$dados = [];
+
+for ($i=0; $i < ; $i++) { 
+    $reserva[i]
+}
+
+$_SESSION['$reserva'] = array(
+    ['tipo_passagem'=>$tipo_passagem, 'id_passageiro'=>$_SESSION['id_passageiro'], 'fk_assento'=>$_SESSION['fk_assento']], //passagem 1
+    ['tipo_passagem'=>$tipo_passagem, 'id_passageiro'=>$_SESSION['id_passageiro'], 'fk_assento'=>$_SESSION['fk_assento']],
+)
+
+$reserva[0]
+?>
