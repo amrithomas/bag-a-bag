fb47d09ec623e2154b579eaa6a5e7ccb5ae7e1a6
Breno-S <114250412+Breno-S@users.noreply.github.com>
2023-04-28 17:19:20 -0300

    Update dados_passagem.php

--- back/dados_passagem.php
+++ back/dados_passagem.php
@@ -2,16 +2,17 @@
 session_start();
 include_once("conexao.php");
 
-$dados = [];
+$dados_passagens = $_SESSION['$dados_passagens'];
 
-for ($i=0; $i < ; $i++) { 
-    $reserva[i]
+$reserva = [];
+
+for ($i=0; $i < sizeof($dados_passagens); $i++) { 
+    $reserva[$i] = $dados_passagens[$i];
 }
 
-$_SESSION['$reserva'] = array(
-    ['tipo_passagem'=>$tipo_passagem, 'id_passageiro'=>$_SESSION['id_passageiro'], 'fk_assento'=>$_SESSION['fk_assento']], //passagem 1
-    ['tipo_passagem'=>$tipo_passagem, 'id_passageiro'=>$_SESSION['id_passageiro'], 'fk_assento'=>$_SESSION['fk_assento']],
-)
+//  = array(
+//     ['tipo_passagem'=>"", 'id_passageiro'=>$_SESSION['id_passageiro'], 'fk_assento'=>$_SESSION['fk_assento']], //passagem 1
+//     ['tipo_passagem'=>$tipo_passagem, 'id_passageiro'=>$_SESSION['id_passageiro'], 'fk_assento'=>$_SESSION['fk_assento']],
+// )
 
-$reserva[0]
 ?>
